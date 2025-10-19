74dd9e8fb2ea70d77c215e4ccb422e67ee97b730
Yaechan Lee <lyc021119@gmail.com>
2025-10-19 23:33:53 +0900

    feat(calculator) add Main()

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import View.Execute;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Execute execute = new Execute();
+        execute.print();
     }
 }
