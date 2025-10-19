f9ca14b6149cd34f9ee2c6ef55439cbcfb05eb73
Yaechan Lee <lyc021119@gmail.com>
2025-10-19 23:16:22 +0900

    feat(Delimiter): add void addDelimiter(String)

---
+++ src/main/java/Delimiter/Delimiter.java
@@ -0,0 +1,19 @@
+package Delimiter;
+
+public class Delimiter {
+    private final String DEFAULT_DELIMITER = ",|:";
+    private String customDelimiter = DEFAULT_DELIMITER;
+
+    public void addDelimiter(String delimiter) {
+        if (delimiter.length() > 1 && !delimiter.equals("default")) {
+            throw new IllegalArgumentException("[ERROR] 구분자로 문자열이 입력되었습니다.");
+        } else if (delimiter.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException("[ERROR] 구분자로 숫자를 입력했습니다.");
+        } else if (delimiter.equals("default")) {
+            customDelimiter = DEFAULT_DELIMITER;
+        } else {
+            customDelimiter += '|' + delimiter;
+        }
+    }
+
+}
