9d5f738f565d56cae30ccda4cae5cf7325f249f8
Yaechan Lee <lyc021119@gmail.com>
2025-10-19 23:21:28 +0900

    feat(delimiterRemover): add String extractNumString(String)

---
+++ src/main/java/DelimiterRemover/DelimiterRemover.java
@@ -0,0 +1,20 @@
+package DelimiterRemover;
+
+import Delimiter.CustomDelimiterParser;
+import Delimiter.Delimiter;
+
+public class DelimiterRemover {
+    Delimiter delimiter = new Delimiter();
+    String delimiter_ = delimiter.getDelimiter();
+
+    CustomDelimiterParser customDelimiterParser = new CustomDelimiterParser();
+
+    public String extractNumString(String input) {
+        if (customDelimiterParser.hasCustomDelimiter(input)) {
+            return input.substring(input.indexOf(customDelimiterParser.getCUSTOM_DELIMITER_SUFFIX()) + 2);
+        } else {
+            return input;
+        }
+    }
+
+}
